ebae9af1e29748820e4ed494d2704c3f095006fe
NickY-bit <66704161+NickY-bit@users.noreply.github.com>
2021-04-03 19:13:56 -0400

    Updated index.html, scripts.js, and styles.css
    Finally got the rain visual to work

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var makeItRain = function() {
   //clear out everything
-  $('.rain').empty();
+  document.getElementById("front-row").innerHTML = "";
+  document.getElementById("back-row").innerHTML = "";
 
   var increment = 0;
   var drops = "";
@@ -20,26 +21,8 @@ var makeItRain = function() {
     backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
   }
 
-  $('.rain.front-row').append(drops);
-  $('.rain.back-row').append(backDrops);
+  document.getElementById("front-row").innerHTML += drops;
+  document.getElementById("back-row").innerHTML += backDrops;
 }
-
-$('.splat-toggle.toggle').on('click', function() {
-  $('body').toggleClass('splat-toggle');
-  $('.splat-toggle.toggle').toggleClass('active');
-  makeItRain();
-});
-
-$('.back-row-toggle.toggle').on('click', function() {
-  $('body').toggleClass('back-row-toggle');
-  $('.back-row-toggle.toggle').toggleClass('active');
-  makeItRain();
-});
-
-$('.single-toggle.toggle').on('click', function() {
-  $('body').toggleClass('single-toggle');
-  $('.single-toggle.toggle').toggleClass('active');
-  makeItRain();
-});
 
 makeItRain();
